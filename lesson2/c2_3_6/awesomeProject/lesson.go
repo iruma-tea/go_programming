1ddfc44821f3e89e5abeb018e5188e8d0a54c98d
iruma-tea <shusuke.think.pad@gmail.com>
2025-01-12 19:08:10 +0900

    2-3 switch文の学習開始・終了

---
+++ lesson2/c2_3_6/awesomeProject/lesson.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func getOsName() string {
+	return "mac"
+}
+
+func main() {
+	switch os := getOsName(); os {
+	case "mac":
+		fmt.Println("Mac!!")
+	case "windows":
+		fmt.Println("Windows!!")
+	}
+}
