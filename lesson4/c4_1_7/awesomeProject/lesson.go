9b56dbe6f18d5847b34a259e306cd758374d712d
iruma-tea <shusuke.think.pad@gmail.com>
2025-01-18 08:37:55 +0900

    4.1 メソッドを作成しようの学習開始・終了

---
+++ lesson4/c4_1_7/awesomeProject/lesson.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+type MyInt int
+
+func (i MyInt) Double() int {
+	// return i * 2 // castしないとエラーになる
+	return int(i * 2)
+}
+
+func main() {
+	myInt := MyInt(10)
+	fmt.Println(myInt.Double())
+}
