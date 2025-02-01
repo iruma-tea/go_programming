dc9af853e63aa61bde5e3cd06ddf3a4fae87bc4c
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-02-02 08:57:03 +0900

    c9_1_1の学習完了

--- lesson9/c9_1_1/go-api-arch-mvc-template/app/models/album_test.go
+++ lesson9/c9_1_1/go-api-arch-mvc-template/app/models/album_test.go
@@ -1,8 +1,10 @@
 package models_test
 
 import (
+	"fmt"
 	"go-api-arch-mvc-template/app/models"
 	"go-api-arch-mvc-template/pkg/tester"
+	"strings"
 	"testing"
 	"time"
 
@@ -37,4 +39,52 @@ func (suite *AlbumTestSuite) TestAlbum() {
 	createAlbum, err := models.CreateAlbum("Test", time.Now(), "sports")
 	suite.Assert().Nil(err)
 	suite.Assert().Equal("Test", createAlbum.Title)
+	suite.Assert().NotNil(createAlbum.ReleaseDate)
+	suite.Assert().NotNil(createAlbum.Category.ID)
+	suite.Assert().Equal(createAlbum.Category.Name, "sports")
+
+	getAlbum, err := models.GetAlbum(createAlbum.ID)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal("Test", getAlbum.Title)
+	suite.Assert().NotNil(getAlbum.ReleaseDate)
+	suite.Assert().NotNil(getAlbum.Category.ID)
+	suite.Assert().Equal(getAlbum.Category.Name, "sports")
+
+	getAlbum.Title = "updated"
+	err = getAlbum.Save()
+	suite.Assert().Nil(err)
+	updatedAlbum, err := models.GetAlbum(createAlbum.ID)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal("updated", updatedAlbum.Title)
+	suite.Assert().NotNil(updatedAlbum.ReleaseDate)
+	suite.Assert().NotNil(updatedAlbum.Category.ID)
+	suite.Assert().Equal(updatedAlbum.Category.Name, "sports")
+
+	err = updatedAlbum.Delete()
+	suite.Assert().Nil(err)
+	deleteAlbum, err := models.GetAlbum(updatedAlbum.ID)
+	suite.Assert().Nil(deleteAlbum)
+	suite.Assert().True(strings.Contains("record not found", err.Error()))
+
+}
+
+func (suite *AlbumTestSuite) TestAlbumMarshal() {
+	album := models.Album{
+		Title:       "Test",
+		ReleaseDate: Str2time("2023-01-01"),
+		Category:    &models.Category{Name: "sports"},
+	}
+
+	anniversary := time.Now().Year() - 2023
+	albumJSON, err := album.MarshalJSON()
+	suite.Assert().Nil(err)
+	suite.Assert().JSONEq(fmt.Sprintf(`{
+		"anniversary":%d,
+		"category":{
+			"id":0, "name":"sports"
+		},
+		"id":0,
+		"releaseDate":"2023-01-01",
+		"title":"Test"
+	}`, anniversary), string(albumJSON))
 }
