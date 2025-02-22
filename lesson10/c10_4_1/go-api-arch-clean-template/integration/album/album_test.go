9dfac86388ed997a52e2634cd17e11e9bd4b0d7e
shusuke-iruma-tea <shusuke.pana@gmail.com>
2025-02-22 20:24:40 +0900

    c10_4_1の実装完了

--- lesson10/c10_4_1/go-api-arch-clean-template/integration/album/album_test.go
+++ lesson10/c10_4_1/go-api-arch-clean-template/integration/album/album_test.go
@@ -2,14 +2,15 @@ package integration
 
 import (
 	"context"
-	"go-api-arch-clean-template/adapter/controller/gin/presenter"
-	"go-api-arch-clean-template/pkg"
 	"net/http"
 	"testing"
 	"time"
 
 	openapi_types "github.com/oapi-codegen/runtime/types"
 	"github.com/stretchr/testify/suite"
+
+	"go-api-arch-clean-template/adapter/controller/gin/presenter"
+	"go-api-arch-clean-template/pkg"
 )
 
 type AlbumTestSuite struct {
@@ -22,8 +23,8 @@ func TestAlbumSuite(t *testing.T) {
 
 func (suite *AlbumTestSuite) TestAlbumCreateGetDelete() {
 	// Create
-	baseEndPoint := pkg.GetEndpoint("/app/v1")
-	apiClient, _ := presenter.NewClientWithResponses(baseEndPoint)
+	baseEndpoint := pkg.GetEndpoint("/api/v1")
+	apiClient, _ := presenter.NewClientWithResponses(baseEndpoint)
 	createResponse, err := apiClient.CreateAlbumWithResponse(context.Background(), presenter.CreateAlbumJSONRequestBody{
 		Title:       "test",
 		Category:    presenter.Category{Name: presenter.Sports},
@@ -35,5 +36,36 @@ func (suite *AlbumTestSuite) TestAlbumCreateGetDelete() {
 	suite.Assert().NotNil(createResponse.JSON201.Id)
 	suite.Assert().Equal("test", createResponse.JSON201.Title)
 	suite.Assert().Equal("sports", string(createResponse.JSON201.Category.Name))
-	suite.Assert().NotNil(createResponse.JSON201.ReleaseDate.Date())
+	suite.Assert().NotNil(createResponse.JSON201.ReleaseDate)
+
+	// Get
+	getResponse, err := apiClient.GetAlbumByIdWithResponse(context.Background(), createResponse.JSON201.Id)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(http.StatusOK, getResponse.StatusCode())
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(createResponse.JSON201.Id, getResponse.JSON200.Id)
+	suite.Assert().Equal("test", getResponse.JSON200.Title)
+	suite.Assert().Equal("sports", string(getResponse.JSON200.Category.Name))
+	suite.Assert().NotNil(getResponse.JSON200.ReleaseDate)
+
+	// Update
+	title := "updated"
+	category := presenter.Category{
+		Name: presenter.Food,
+	}
+	updateResponse, err := apiClient.UpdateAlbumByIdWithResponse(context.Background(), getResponse.JSON200.Id, presenter.UpdateAlbumByIdJSONRequestBody{
+		Title:    &title,
+		Category: &category,
+	})
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(http.StatusOK, updateResponse.StatusCode())
+	suite.Assert().Nil(err)
+	suite.Assert().Equal("updated", updateResponse.JSON200.Title)
+	suite.Assert().Equal("food", string(updateResponse.JSON200.Category.Name))
+	suite.Assert().NotNil(updateResponse.JSON200.ReleaseDate)
+
+	// Delete
+	deleteResponse, err := apiClient.DeleteAlbumByIdWithResponse(context.Background(), updateResponse.JSON200.Id)
+	suite.Assert().Nil(err)
+	suite.Assert().Equal(http.StatusNoContent, deleteResponse.StatusCode())
 }
